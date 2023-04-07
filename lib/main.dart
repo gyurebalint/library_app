885755d810650d1477de19e131c3e1c946f90289
somakanyasi <kanyasi.soma@gmail.com>
2023-04-07 20:00:45 +0200

    basic library_home_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screens/library_home_screen.dart';
 
 void main() => runApp(MyApp());
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
---
+++ lib/screens/library_home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LibraryHomeScreen extends StatefulWidget {
+  @override
+  State<LibraryHomeScreen> createState() => _LibraryHomeScreenState();
+}
+
+class _LibraryHomeScreenState extends State<LibraryHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Library',
+        ),
+      ),
+    );
+  }
+}
